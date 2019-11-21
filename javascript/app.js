671ab68ba56011f2087082a719ad1029272c8d5d
Hyejin Kim <cshjkin@gmail.com>
2019-11-20 20:03:58 -0800

    add firebase on app.js

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXmM3HuZeoMyp5IZFsUqCurdFncuFhbDQ",
+    authDomain: "my-awesome-project-cshjnim.firebaseapp.com",
+    databaseURL: "https://my-awesome-project-cshjnim.firebaseio.com",
+    projectId: "my-awesome-project-cshjnim",
+    storageBucket: "my-awesome-project-cshjnim.appspot.com",
+    messagingSenderId: "600791751939",
+    appId: "1:600791751939:web:0a9b8cde3f9faf2e12dd36",
+    measurementId: "G-XLM2XPJ8T2"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
+  var database = firebase.database();
+
+
