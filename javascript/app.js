1b3e1d275da10de0773f3a78d8558049d42dbb4b
Hyejin Kim <cshjkin@gmail.com>
2019-11-20 20:46:08 -0800

    add train add button function on js

--- javascript/app.js
+++ javascript/app.js
@@ -13,4 +13,42 @@ var firebaseConfig = {
   
   var database = firebase.database();
 
+  //make button works for adding train
+$("#add-train-btn").on("click", function(event) {
+   event.preventDefault();
+  
+    // Grabs user input
+    var traName = $("#train-name-input").val().trim();
+    var traDest = $("#destination-input").val().trim();
+    var traFre = $("#fre-input").val().trim();
+    var traTime = moment($("#time-input").val().trim(), "HH:mm").format("X");
+    // using built-in methods
+    var date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+    // request a weekday along with a long date
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    console.log(date.toLocaleString('de-DE', options));
 
+    // Creates local "temporary" object for holding employee data
+    var newTra = {
+        name: traName,
+        destination: traDest,
+        frequency: traFre,
+        time: traTime,
+    };
+    // Uploads employee data to the database
+    database.ref().push(newTra);
+  
+    // Logs everything to console
+    console.log(newTra.name);
+    console.log(newTra.destination);
+    console.log(newTra.frequency);
+    console.log(newTra.time);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#fre-input").val("");
+    $("#time-input").val("");
+});
